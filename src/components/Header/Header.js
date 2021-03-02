90cd8a35c43d74360f858df38d947ddba4ff0f22
Imari Pickens <imaridp23@gmail.com>
2021-03-02 18:05:46 -0500

    Adding styling to header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,9 +4,9 @@ import Jumbotron from 'react-bootstrap/Jumbotron';
 
 function Header() {
     return (
-        <Jumbotron fluid>
-            <Container>
-                <h1>Employee Directory</h1>
+        <Jumbotron fluid className="bg-secondary">
+            <Container className="text-center">
+                <h1 className="h1 text-white">Employee Directory</h1>
             </Container>
         </Jumbotron>
     )
