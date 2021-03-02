68fbeba09685d59aa14ebfc225368a1e8fb96e4e
Imari Pickens <imaridp23@gmail.com>
2021-03-02 17:57:52 -0500

    Adding simple footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <footer>
+            <br></br>
+            <p>© Imari Pickens 2021</p>
+        </footer>
+    );
+};
+
+export default Footer;
